3936c0caae43aa4138d0c0aa5e8dcf1ad4f70171
HungryOwl <ptica-govorun@inbox.ru>
2017-01-18 01:23:25 +0300

    Revert "Сделать сборку"
    This reverts commit 0e9415350a188cf216aa7451c2c355e598bbe7b5.

--- gulpfile.js
+++ gulpfile.js
@@ -5,57 +5,10 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var minify = require('gulp-csso');
-var imagemin = require('gulp-imagemin');
-var rename = require("gulp-rename");
-var run = require("run-sequence");
 var server = require("browser-sync");
 var svgstore = require("gulp-svgstore");
+//var svgmin = require("gulp-svgmin");
 var rename = require("gulp-rename");
-var del = require("del");
-
-gulp.task("clean", function() {
-  return del("build");
-});
-
-gulp.task("copy-html", function() {
-  return gulp.src([
-    "*.html"
-    ], {
-      base: "."
-    })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("copy", function() {
-  return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html"
-    ], {
-      base: "."
-    })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("symbols", function() {
-  return gulp.src("build/img/icons/*.svg")
-  .pipe(svgstore({
-    inlineSvg: true
-  }))
-  .pipe(rename("symbols.svg"))
-  .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("images", function() {
-  return gulp.src("build/img/**/*.{png,jpg,gif}")
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true})
-    ]))
-  .pipe(gulp.dest("build/img"));
-});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -72,32 +25,28 @@ gulp.task("style", function() {
         "last 2 Edge versions"
       ]})
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream())
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
+    .pipe(gulp.dest("css"))
+    .pipe(server.reload({stream: true}));
 });
 
-gulp.task("serve", function() {
+gulp.task("symbols", function() {
+  return gulp.src("img/icons/*.svg")
+  //.pipe(svgmin())
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("symbols.svg"))
+  .pipe(gulp.dest("img"));
+});
+
+gulp.task("serve", ["style"], function() {
   server.init({
-    server: "build",
+    server: ".",
     notify: false,
     open: true,
     ui: false
   });
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html", ["copy-html"]).on("change", server.reload);
-});
-
-gulp.task("build", function(fn) {
-  run(
-    "clean",
-    "copy",
-    "style",
-    "images",
-    "symbols",
-    fn
-    );
+  gulp.watch("*.html").on("change", server.reload);
 });
